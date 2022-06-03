3a77918e207d66fabe8781fa48f4df1d09cb88e1
pingu <giorgiosacchi@me.com>
2022-06-03 03:56:33 +0200

    Changed buckets, d, made list comprahension and opt prints

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import math
 import statistics
 import sys
 import random
+import time
 """
 We have understood the requirement 300(b+1) integer memory limit refer to the bucket counters and constants of 
 hash/sign functions, i.e. what is printed and recuperated. Thus not including integers like s, n, t, b
 """
 
 def main():
-    random.seed(300)
+    random.seed(5)
     s, n, t, b = 0, None, 0, 0
     #lines = sys.stdin
     for line in sys.stdin:
@@ -21,24 +22,20 @@ def main():
             b = int(l[2])
             break
     # decide what this should be, num of buckets
-    bucks = 7*b
+    bucks = 5*(b+1)
+    if n/bucks > 100:
+        missing = n//100 - bucks
+        bucks += int(1.02*missing)
     # number of hash fucs. delta and c to be decided
-    d = math.floor(300*(b+1)/(6+bucks))
+    d = math.floor(200*(b+1)/(6+bucks))
     # now choosing the first abitrarly
-    p = find_prim(10**8)
-
-    if p is None:
-        #print("bajs")
-        pass
+    #p = find_prim(10**8)
+    p = 100000007
 
     counter = 0
     if s == 1:
         bucket_counters = [[0 for _ in range(bucks)] for _ in range(d)]  # d x bucks
         hash_list, sign_list = generate_ind_hash(p, d)
-
-        if hash_list is None:
-            #print("bajs")
-            pass
 
         for line in sys.stdin:
             l = line.rstrip().split(" ")
@@ -46,7 +43,7 @@ def main():
             score = int(l[1])
             for i in range(d):
                 hash_f = func_hash(hash_list[i], id, p, bucks)
-                sign_f = sign_hash(hash_list[i], id, p)
+                sign_f = sign_hash(sign_list[i], id, p)
                 # minus because I want to take yi - xi and this is xi
                 bucket_counters[i][hash_f] -= sign_f*score
             counter += 1
@@ -55,16 +52,11 @@ def main():
 
         # output
         print(d*(6+bucks))
-        for i in range(d):
-            for j in range(bucks):
-                print(int(bucket_counters[i][j]), end=" ")
+        flat_string_buckets = " ".join([str(int(bucket_counters[i][j])) for i in range(d) for j in range(bucks)])
+        print(flat_string_buckets, end=" ")
+        hashes_string = " ".join([hash_list[i].for_print() + " " + sign_list[i].for_print() for i in range(d)])
+        print(hashes_string)
 
-        for i in range(d):
-            print(hash_list[i].for_print(), end=" ")
-            if i == d:
-                print(sign_list[i].for_print())
-            else:
-                print(sign_list[i].for_print(), end=" ")
     else:
         m = 0
         bucket_counters = []
@@ -79,49 +71,49 @@ def main():
                 l = line.rstrip().split(" ")
                 bucket_vals = l[:bucks*d]
                 hashing_vals = l[bucks*d:]
-                for i in range(d):
-                    bucket_counters.append([])
-                    for j in range(bucks):
-                        bucket_counters[-1].append(int(bucket_vals[i*bucks + j]))
+                bucket_counters = [[int(bucket_vals[i*bucks + j]) for j in range(bucks)] for i in range(d)]
                 hash_list, sign_list = hash_s2(hashing_vals, d)
-            else:
-                l = line.rstrip().split(" ")
-                id = int(l[0])
-                score = int(l[1])
-                for i in range(d):
-                    hash_f = func_hash(hash_list[i], id, p, bucks)
-                    sign_f = sign_hash(hash_list[i], id, p)
-                    # plus because I want to take yi - xi and this is yi
-                    bucket_counters[i][hash_f] += sign_f * score
-                counter += 1
-                if counter == n:
-                    break
+                break
+
+        for line in sys.stdin:
+            l = line.rstrip().split(" ")
+            id = int(l[0])
+            score = int(l[1])
+            for i in range(d):
+                hash_f = func_hash(hash_list[i], id, p, bucks)
+                sign_f = sign_hash(sign_list[i], id, p)
+                # plus because I want to take yi - xi and this is yi
+                bucket_counters[i][hash_f] += sign_f * score
+            counter += 1
+            if counter == n:
+                break
 
         # sequential queries now
         k = None
-        #counter = 0
+        counter = 0
         for line in sys.stdin:
             l = line.rstrip().split(" ")
             if k is None:
                 k = int(l[0])
                 continue
-            approx = []
             id = int(l[0])
+            approx = []
             for i in range(d):
                 hash_f = func_hash(hash_list[i], id, p, bucks)
-                sign_f = sign_hash(hash_list[i], id, p)
-                # plus because I want to take yi - xi and this is yi
+                sign_f = sign_hash(sign_list[i], id, p)
                 approx.append(sign_f * bucket_counters[i][hash_f])
-            #alt. sort and take middle elem
+
+            #approx = [sign_hash(sign_list[i], id, p) * bucket_counters[i][func_hash(hash_list[i], id, p, bucks)] for i in range(d)]
+
             med = statistics.median(approx)
             if med >= t/2:
                 print("Yes")
             else:
                 print("No")
             sys.stdout.flush()
-            #counter += 1
-            #if counter >= k:
-                #break
+            counter += 1
+            if counter >= k:
+                break
 
 def find_prim(n):
     """first prim between n and 2n"""
@@ -145,36 +137,21 @@ def find_prim(n):
     return None
 
 def generate_ind_hash(p, d):
-    h_a = []
-    h_b = []
-    s_a = []
-    s_b = []
-    s_c = []
-    s_d = []
-    for i in range(d):
-        h_a.append(random.randint(1, p-1))
-        h_b.append((random.randint(0, p-1)))
-        s_a.append(random.randint(1, p-1))
-        s_b.append((random.randint(0, p-1)))
-        s_c.append((random.randint(0, p-1)))
-        s_d.append((random.randint(0, p-1)))
-    #check none are equal
-    """if (len(h_a) != len(set(h_a)) and len(h_b) != len(set(h_b))) or (len(s_a) != len(set(s_a)) and len(s_b) != len(set(s_b))
-                                                        and len(s_c) != len(set(s_c)) and len(s_d) != len(set(s_d))):
-        return None, None"""
+    h_a = [random.randint(1, p-1) for _ in range(d)]
+    h_b = [random.randint(0, p - 1) for _ in range(d)]
+    s_a = [random.randint(1, p - 1) for _ in range(d)]
+    s_b = [random.randint(0, p - 1) for _ in range(d)]
+    s_c = [random.randint(0, p - 1) for _ in range(d)]
+    s_d = [random.randint(0, p - 1) for _ in range(d)]
     return hash(h_a, h_b, s_a, s_b, s_c, s_d, d)
 
 def func_hash(hash_vals, value, p, bucks):
-    s = 0
-    for i, const in enumerate(hash_vals.vars):
-        s += const*value**i
-    return (s % p) % bucks
+    a, b = hash_vals.vars
+    return ((a*value + b) % p) % bucks
 
 def sign_hash(hash_vals, value, p):
-    s = 0
-    for i, const in enumerate(hash_vals.vars):
-        s += const * value ** i
-    return (((s % p) % 2) - 0.5) * 2
+    a, b, c, d = hash_vals.vars
+    return ((((a*value**3 + b*value**2 + c*value + d) % p) % 2) - 0.5) * 2
 
 def hash(h_a, h_b, s_a, s_b, s_c, s_d, nr_hash):
     hash_list = []
@@ -188,6 +165,8 @@ def hash(h_a, h_b, s_a, s_b, s_c, s_d, nr_hash):
         c = s_c[i]
         d = s_d[i]
         sign_list.append(Hashes([a, b, c, d]))
+    #hash_list = [Hashes([h_a[i], h_b[i]]) for i in range(nr_hash)]
+    #sign_list = [Hashes([s_a[i], s_b[i], s_c[i], s_d[i]]) for i in range(nr_hash)]
     return hash_list, sign_list
 
 def hash_s2(hash_vals, nr_hash):
@@ -197,6 +176,8 @@ def hash_s2(hash_vals, nr_hash):
         vals = hash_vals[i*6:(i+1)*6]
         hash_list.append(Hashes([int(val) for val in vals[:2]]))
         sign_list.append(Hashes([int(val) for val in vals[2:]]))
+    #hash_list_2 = [Hashes([int(val) for val in hash_vals[i*6:i*6 + 2]]) for i in range(nr_hash)]
+    #sign_list = [Hashes([int(val) for val in hash_vals[i * 6 + 2:(i + 1) * 6]]) for i in range(nr_hash)]
     return hash_list, sign_list
 
 class Hashes:
